7af9472d7486ae1601466d24eb4a937593cc39d4
jerry <jerrynwosu007@gmail.com>
2023-03-15 00:32:23 +0100

    working on add edit label

--- src/components/AddEditLabels.jsx
+++ src/components/AddEditLabels.jsx
@@ -1,25 +1,50 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { toggleAddEditLabel } from '../features/action/actionSlice';
 import { createLabel } from '../features/label/labelSlice';
-import { Overlay } from '../styles/GlobalStyles';
+import { ButtonIconSm, Overlay } from '../styles/GlobalStyles';
+import { BinIcon, PaperIcon, PlaneIcon } from './icons';
 
 const AddEditLabels = () => {
   const dispatch = useDispatch();
   const { labels } = useSelector((state) => state.label);
   const { addLabelModal } = useSelector((state) => state.action);
+  const [editingIndex, setEditingIndex] = useState(-1);
+  const [editedText, setEditedText] = useState('');
   const [name, setName] = useState('');
+  const inputRef = useRef();
 
   const handleCreateLabel = (e) => {
     e.preventDefault();
     dispatch(createLabel({ name }));
   };
 
+  const handleItemClick = (index, text) => {
+    setEditingIndex(index);
+    setEditedText(text);
+  };
+
+  const handleInputBlur = () => {
+    setEditingIndex(-1);
+    setEditedText('');
+  };
+
+  const handleInputChange = (event) => {
+    setEditedText(event.target.value);
+  };
+
   const closeModal = () => {
     dispatch(toggleAddEditLabel(false));
   };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editingIndex]);
+
   return (
     <AnimatePresence>
       {addLabelModal && (
@@ -39,17 +64,50 @@ const AddEditLabels = () => {
             exit={{ scale: 0, y: 300 }}
             transition={{ duration: 0.5 }}
           >
-            <div>
+            <Inner>
               <form onSubmit={handleCreateLabel}>
-                <input
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder="Create new label"
-                />
-                <button>create</button>
+                <NewLabel>
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Create new label"
+                  />
+                  <ButtonIconSm>
+                    <PlaneIcon />
+                  </ButtonIconSm>
+                </NewLabel>
               </form>
-            </div>
+              <LabelList>
+                <ul>
+                  {labels.map((label, index) => (
+                    <li key={label.id}>
+                      <ButtonIconSm>
+                        <BinIcon />
+                      </ButtonIconSm>
+                      {editingIndex === index ? (
+                        <input
+                          type="text"
+                          value={editedText}
+                          onChange={handleInputChange}
+                          onBlur={handleInputBlur}
+                          ref={inputRef}
+                        />
+                      ) : (
+                        <span
+                          onClick={() => handleItemClick(index, label.name)}
+                        >
+                          {label.name}
+                        </span>
+                      )}
+                      <ButtonIconSm>
+                        <PlaneIcon />
+                      </ButtonIconSm>
+                    </li>
+                  ))}
+                </ul>
+              </LabelList>
+            </Inner>
           </Main>
         </AddEditLabelsWrap>
       )}
@@ -76,4 +134,55 @@ const Main = styled.div`
   background-color: #fff;
   width: 300px;
   border-radius: 21px;
+  overflow: hidden;
+`;
+
+const Inner = styled.div`
+  overflow-y: auto;
+  height: 100%;
+  max-height: 80vh;
+`;
+
+const LabelList = styled.div`
+  padding-bottom: 20px;
+  ul {
+    list-style: none;
+    li {
+      font-size: 16px;
+      display: flex;
+      color: #555;
+      cursor: text;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0px 20px;
+      gap: 10px;
+    }
+    input,
+    span {
+      font-size: 14px;
+      text-align: left;
+      width: 65%;
+      font-weight: 400;
+      color: inherit;
+    }
+    input {
+      border: none;
+      outline: none;
+      border-bottom: 1px solid #999;
+    }
+  }
+`;
+
+const NewLabel = styled.div`
+  display: flex;
+  padding: 30px 20px;
+  flex: 1;
+
+  input {
+    flex: 1;
+    border: none;
+    outline: none;
+    border-bottom: 1px solid #999;
+    font-size: 14px;
+  }
 `;
